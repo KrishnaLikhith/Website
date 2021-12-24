b23763d464c5cd70c955f6f02f54f9fef6d7294e
KrishnaLikhith <76393325+KrishnaLikhith@users.noreply.github.com>
2021-12-24 21:02:53 +0530

    Add files via upload

---
+++ draw.js
@@ -0,0 +1,36 @@
+function Back() {
+    window.location = "art.html";
+}
+function caricature() {
+    window.location = "caricature.html";
+}
+function cartoon() {
+    window.location = "cartoon.html";
+}
+function figure() {
+    window.location = "figure.html";
+}
+function gesture() {
+    window.location = "gesture.html";
+}
+function line() {
+    window.location = "line.html";
+}
+function photo() {
+    window.location = "photo.html";
+}
+function point() {
+    window.location = "point.html";
+}
+function sci(){
+    window.location = "sci.html";
+}
+function scr(){
+    window.location = "scr.html";
+}
+function sil(){
+    window.location = "sil.html";
+}
+function ske(){
+    window.location = "ske.html";
+}
---
+++ main.js
@@ -0,0 +1,21 @@
+function Art() {
+    window.location = "art.html";
+}
+function draw() {
+    window.location = "draw.html";
+}
+function paint() {
+    window.location = "paint.html";
+}
+function addart() {
+    window.location = "addart.html";
+}
+function Back(){
+    window.location = "courses.html";
+}
+function dance() {
+    window.location = "dance.html";
+}
+function course(){
+    window.location = "courses.html";
+}
